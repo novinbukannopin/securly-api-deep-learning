c3e53249b704d6c23697a89d54a3d0e200b42535
novinbukannopin <novinardiany@outlook.com>
2024-12-16 20:58:03 +0700

    feat: add db trust

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ load_dotenv()
 MODEL_PATH = os.getenv("MODEL_PATH", "files/600-32.pth")
 BERT_PATH = os.getenv("BERT_PATH", "files/charbert-bert-wiki")
 VOCAB_FILE = os.getenv("VOCAB_FILE", "files/charbert-bert-wiki/vocab.txt")
+BLOCKLIST_FILE = os.getenv("BLOCKLIST_FILE", "files/blocklist.txt")
 
 DEVICE = torch.device("cuda" if os.getenv("DEVICE", "cpu") == "cuda" and torch.cuda.is_available() else "cpu")
 
