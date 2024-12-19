3ad39c72ccc60f896d6712dbbf67f43b7fa8123a
novinbukannopin <novinardiany@outlook.com>
2024-12-19 20:43:31 +0700

    feat: rate limiter in routes

---
+++ app/routes/__init__.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+from .health import health_routes
+from .predict import predict_routes
+
+def setup_routes(app, limiter, redis_client):
+    bp = Blueprint("routes", __name__)
+
+    health_routes(bp, limiter, redis_client)
+
+    predict_routes(bp, limiter, redis_client)
+
+    app.register_blueprint(bp)
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask
-from app.routes import bp
+from core.limiter import LimiterService
+from app.routes import setup_routes
 
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(bp)
-    return app
+app = Flask(__name__)
+
+limiter_service = LimiterService(app)
+limiter = limiter_service.get_limiter()
+redis_client = limiter_service.get_redis_client()
+
+setup_routes(app, limiter, redis_client)
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
